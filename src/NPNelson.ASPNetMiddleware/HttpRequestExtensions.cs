25f0ab1d3d0f247864c0478d66738590bf89140c
Nicholas P. Nelson <nnelson1@insight.rr.com>
2017-04-03 12:06:54 -0400

    initial code commit

---
+++ src/NPNelson.ASPNetMiddleware/HttpRequestExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Http
+{
+    //http://www.strathweb.com/2016/04/request-islocal-in-asp-net-core/
+
+    public static class HttpRequestExtensions
+    {
+        /// <summary>
+        /// Determines if a request originated from localhost
+        /// DANGER: Be careful when running in reverse proxy scenarioes. You must have middleware running that understands the X-Forwarded headers to set the connection information, otherwise you will get the IP address of the proxy
+        /// See http://www.strathweb.com/2016/04/request-islocal-in-asp-net-core/ for more details
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        public static bool IsLocal(this HttpRequest req)
+        {
+            var connection = req.HttpContext.Connection;
+            if (connection.RemoteIpAddress != null)
+            {
+                if (connection.LocalIpAddress != null)
+                {
+                    return connection.RemoteIpAddress.Equals(connection.LocalIpAddress);
+                }
+                else
+                {
+                    return IPAddress.IsLoopback(connection.RemoteIpAddress);
+                }
+            }
+
+            // for in memory TestServer or when dealing with default connection info
+            if (connection.RemoteIpAddress == null && connection.LocalIpAddress == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ test/NPNelson.ASPNetMiddleware.Tests/HttpRequestExtensionTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace NPNelson.ASPNetMiddleware.Tests
+{
+    public class HttpRequestExtensionTests
+    {
+        [Theory]
+        [InlineData(null,null,true)]
+        [InlineData("192.168.1.1", null, false)] //remoteIP is not localhost
+        [InlineData("127.0.0.1", null, true)] //remoteIP is localhost
+        [InlineData(null, "127.0.0.1", false)] //if the remoteIP doesn't come across, we want to ignore the localIP (unless localIP is null)
+        [InlineData(null, "192.168.1.1", false)]//same here, if the remoteIP doesn't come across, we want to ignore the localIP (unless localIP is null)
+        [InlineData("192.168.1.1", "192.168.1.1", true)] //when remoteIP and localIP are same, it can be considered a localrequest regardless of the address
+        [InlineData("192.168.1.1", "192.168.1.2", false)] //obviously not localhost
+        [InlineData("192.168.1.1", "127.0.0.1", false)] //again, if remoteIP is not null and not localhost, it is not a localrequest
+        [InlineData("127.0.0.1", "127.0.0.1", true)] //obviously a localrequest
+        public void LocalHostTest(string remoteIPAddress,string localIPAddress, bool isLocal)
+        {
+            var context = new DefaultHttpContext();
+            context.Connection.RemoteIpAddress = GetIPAddressFromString(remoteIPAddress);
+            context.Connection.LocalIpAddress = GetIPAddressFromString(localIPAddress);
+
+            var result = context.Request.IsLocal();
+
+            result.Should().Be(isLocal,$"Remote IP={remoteIPAddress} LocalIP={localIPAddress}");
+        }
+
+        private IPAddress GetIPAddressFromString(string ipAddress)
+        {
+            if (ipAddress == null) return null;
+            return IPAddress.Parse(ipAddress);
+        }
+     
+    }
+}
